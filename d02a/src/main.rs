8b1cd7eb1c280c922c75bcda9d2abedfd1b6df1d
multi prise <korokoko.toi@gmail.com>
2024-12-20 22:31:22 +0100

    formatting and typos

--- d02a/src/main.rs
+++ d02a/src/main.rs
@@ -1,19 +1,29 @@
 fn is_safely_increasing(report: &[i32]) -> bool {
-    report.windows(2).all(|pair| pair[0] < pair[1] && pair[1] - pair[0] <= 3)
+    report
+        .windows(2)
+        .all(|pair| pair[0] < pair[1] && pair[1] - pair[0] <= 3)
 }
 
 fn is_safely_decreasing(report: &[i32]) -> bool {
-    report.windows(2).all(|pair| pair[0] > pair[1] && pair[0] - pair[1] <= 3)
+    report
+        .windows(2)
+        .all(|pair| pair[0] > pair[1] && pair[0] - pair[1] <= 3)
 }
 
-
 fn main() {
-    let reports =  include_str!("../input.txt")
+    let reports = include_str!("../input.txt")
         .lines()
-        .map(|line| line.split_whitespace().map(|x| x.parse::<i32>().unwrap()).collect::<Vec<_>>())
+        .map(|line| {
+            line.split_whitespace()
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect::<Vec<_>>()
+        })
         .collect::<Vec<_>>();
 
-    let safe_reports = reports.clone().into_iter()
-        .filter(|report| is_safely_decreasing(report) || is_safely_increasing(report)).collect::<Vec<_>>();
+    let safe_reports = reports
+        .clone()
+        .into_iter()
+        .filter(|report| is_safely_decreasing(report) || is_safely_increasing(report))
+        .collect::<Vec<_>>();
     println!("{:?}", safe_reports.len())
 }
