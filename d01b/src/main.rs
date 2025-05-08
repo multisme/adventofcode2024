c525bda15df3e5e7fc1dd97ebadba3249c4b33ea
multi prise <korokoko.toi@gmail.com>
2025-05-08 16:34:55 +0200

    small factorisation d01b

--- d01b/src/main.rs
+++ d01b/src/main.rs
@@ -4,17 +4,15 @@ fn main() {
         .map(|x| x.parse::<i32>().unwrap())
         .enumerate()
         .partition(|(index, _)| index % 2 == 0);
-    let mut left = left
+    let left = left
         .into_iter()
-        .map(|(_, value)| value)
-        .collect::<Vec<i32>>();
+        .map(|(_, value)| value);
     let right = right
         .into_iter()
         .map(|(_, value)| value);
     
     let res = left
-        .iter()
-        .map( |a| right.clone().filter(|x| x == a).count() as i32 * *a)
+        .map( |a| right.clone().filter(|x| *x == a).count() as i32 * a)
         .sum::<i32>();
     println!("{}", res)
 }
