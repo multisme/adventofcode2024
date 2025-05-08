bedaa4e0848a1a87cf40877cbed806d5702796d3
multi prise <korokoko.toi@gmail.com>
2025-05-08 16:27:37 +0200

    small factorisation d01a

--- d01a/src/main.rs
+++ d01a/src/main.rs
@@ -1,22 +1,22 @@
+fn compute_list(list: Vec<(usize, i32)>) -> Vec<i32> {
+    let mut new_list = list
+        .into_iter()
+        .map(|(_, value)| value)
+        .collect::<Vec<i32>>();
+    new_list.sort();
+    new_list
+}
+
 fn main() {
     let (left, right): (Vec<_>, Vec<_>) = include_str!("../input.txt")
         .split_whitespace()
         .map(|x| x.parse::<i32>().unwrap())
         .enumerate()
         .partition(|(index, _)| index % 2 == 0);
-    let mut left = left
-        .into_iter()
-        .map(|(_, value)| value)
-        .collect::<Vec<i32>>();
-    let mut right = right
-        .into_iter()
-        .map(|(_, value)| value)
-        .collect::<Vec<i32>>();
-    left.sort();
-    right.sort();
-    let res = left
+
+    let res = compute_list(left)
         .iter()
-        .zip(right.iter())
+        .zip(compute_list(right))
         .map(|(a, b)| (a - b).abs())
         .sum::<i32>();
     println!("{}", res)
