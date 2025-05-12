f8dbc09c466f850e2674c04db049432baf0936d5
multi prise <korokoko.toi@gmail.com>
2025-05-12 17:50:59 +0200

    exercice d07a

---
+++ d07a/src/main.rs
@@ -0,0 +1,37 @@
+struct Calibration {
+    result: i64,
+    values: Vec<i64>,
+}
+
+fn test_calibration(result: i64, acc: i64, values: &[i64]) -> bool {
+    if acc > result || (values.is_empty() && acc != result) {
+        return false;
+    }
+    if values.is_empty() {
+        return true;
+    }
+    test_calibration(result, acc + values[0], &values[1..])
+        || test_calibration(result, acc * values[0], &values[1..])
+}
+
+fn main() {
+    let data = include_str!("../input.txt");
+
+    let calibrations_result = data
+        .lines()
+        .map(|x| {
+            let mut line = x.split(": ");
+            Calibration {
+                result: line.next().unwrap().parse().unwrap(),
+                values: line
+                    .next()
+                    .unwrap()
+                    .split_whitespace()
+                    .map(|v| v.parse::<i64>().unwrap())
+                    .collect::<Vec<i64>>(),
+            }
+        })
+        .filter(|x| test_calibration(x.result, x.values[0], &x.values[1..]))
+        .fold(0, |acc, x| acc + x.result);
+    println!("{calibrations_result}");
+}
